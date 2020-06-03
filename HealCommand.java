d0a5fbbb9d6ec6298ca4bf08c2aa0ee4338e981e
Axolxtl <66361953+Axolxtl@users.noreply.github.com>
2020-06-03 22:45:55 +0200

    Cleaning up a few
    I made a few improvements, this is my first plugin I programmed

--- HealCommand.java
+++ HealCommand.java
@@ -12,9 +12,9 @@ public class HealCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if(sender instanceof Player) {
 			Player player = (Player) sender;
-			if(player.hasPermission("HealCommand.heal")) {
+			if(player.hasPermission("healCommand.heal")) {
 				if(args.length == 0) {
-					player.sendMessage("§aYou have been successfully healed!");
+					player.sendMessage("Â§aYou have been successfully healed!");
 					player.setFoodLevel(20);
 					player.setHealth(20);
 				
@@ -24,19 +24,23 @@ public class HealCommand implements CommandExecutor {
 					if(target != null) {
 						target.setHealth(20);
 						target.setFoodLevel(20);
-						target.sendMessage("§aYou were healed!");
-						player.sendMessage("§aYou healed the Player §6" + target.getPlayerListName());
-					} else 
-						player.sendMessage("§cThe player §6" + args[0] + " §cis not on the server!");
-				} else 
-					player.sendMessage("§cPlease use §6/heal <Player>§c!");
+						target.sendMessage("Â§aYou were healed!");
+						player.sendMessage("Â§aYou healed the Player Â§6" + target.getPlayerListName());
+					} else {
+						player.sendMessage("Â§cThe player Â§6" + args[0] + " Â§cis not on the server!");
+					}
+				} else { 
+					player.sendMessage("Â§cPlease use Â§6/heal <Player>Â§c!");
+				}
 					
 					
-			} else
-				player.sendMessage("§cYou have no right to do this!");
+			} else {
+				player.sendMessage("Â§cYou have no right to do this!"); 
+				}
 		  
-		} else 
-			sender.sendMessage("§cYou can only use this command as a player!");
+		} else {
+			sender.sendMessage("Â§cYou can only use this command as a player!");
+		}
 			
 		    
 
